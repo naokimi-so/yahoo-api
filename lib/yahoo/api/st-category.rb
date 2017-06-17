c0f15cecf58e9dd0bb8c9edb54721d584c6eea01
NaokiSHIMAI <shimai@planningdev.com>
2017-06-18 04:11:15 +0900

    StCategoryに対応させた

---
+++ lib/yahoo/api/st-category.rb
@@ -0,0 +1,26 @@
+module Yahoo
+
+  class Api
+
+    class StCategory
+
+      GetStCategory = "Yahoo::Api::Shp.get_st_category"
+      StCategoryList = "Yahoo::Api::Shp.st_category_list"
+
+      class << self
+
+        def get_st_category(opts={})
+          Yahoo::Request.get("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/getStCategory", Yahoo::Api.merge_pro(opts), "xml")
+        end
+
+        def st_category_list(opts={})
+          Yahoo::Request.get("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/stCategoryList", Yahoo::Api.merge_pro(opts), "xml")
+        end
+
+      end
+
+    end
+
+  end
+
+end
