f112ca696fb834047f5aea6a4d0fe4fa84de30ad
NaokiSHIMAI <shimai@planningdev.com>
2017-06-19 03:59:08 +0900

    Product.rbにpostの処理を追加した

--- lib/yahoo/api/product.rb
+++ lib/yahoo/api/product.rb
@@ -7,6 +7,13 @@ module Yahoo
       MyItemList = "Yahoo::Api::Product.my_item_list"
       GetItem = "Yahoo::Api::Product.get_item"
       SubmitItem = "Yahoo::Api::Product.submit_item"
+      EditItem = "Yahoo::Api::Product.edit_item"
+      MoveItems = "Yahoo::Api::Product.move_items"
+      UpdateItems = "Yahoo::Api::Product.update_items"
+      SetItemDisplayPriority = "Yahoo::Api::Product.set_item_display_priority"
+      DeleteItem = "Yahoo::Api::Product.delete_item"
+      UploadItemFile = "Yahoo::Api::Product.upload_item_file"
+
 
       class << self
 
@@ -20,6 +27,30 @@ module Yahoo
 
         def get_item(opts={})
           Yahoo::Request.get("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/getItem", Yahoo::Api.merge_pro(opts), "xml")
+        end
+
+        def edit_item(opts={})
+          Yahoo::Request.post("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/editItem", Yahoo::Api.merge_pro(opts), "xml")
+        end
+
+        def move_items(opts={})
+          Yahoo::Request.post("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/moveItems", Yahoo::Api.merge_pro(opts), "xml")
+        end
+
+        def update_items(opts={})
+          Yahoo::Request.post("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/updateItems", Yahoo::Api.merge_pro(opts), "xml")
+        end
+
+        def set_item_display_priority(opts={})
+          Yahoo::Request.post("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/setItemDisplayPriority", Yahoo::Api.merge_pro(opts), "xml")
+        end
+
+        def delete_item(opts={})
+          Yahoo::Request.post("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/deleteItem", Yahoo::Api.merge_pro(opts), "xml")
+        end
+
+        def upload_item_file(opts={})
+          Yahoo::Request.post("https://circus.shopping.yahooapis.jp/ShoppingWebService/V1/uploadItemFile", Yahoo::Api.merge_pro(opts), "xml")
         end
       end
 
